da2d6bbb668c032536650eae8e5e52b9610350e9
andresjpg2000 <andresjpg2000@gmail.com>
2025-04-16 11:03:08 +0100

    Refactor navigation and authentication flow; remove unused components and add login/dashboard routes

---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1 class="dashboard">This is the dashboard page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  @media (min-width: 1024px) {
+    .dashboard {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+
+</style>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1 class="login">This is the login page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  @media (min-width: 1024px) {
+    .login {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+
+</style>
