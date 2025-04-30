1d3c314fb94a0ce86b32c72d29e5953d01468b7f
andresjpg2000 <andresjpg2000@gmail.com>
2025-04-30 15:21:34 +0100

    Fix import paths for energy returns controller and model

--- server/routes/energy-returns.routes.js
+++ server/routes/energy-returns.routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-const energyReturnsController = require('../controllers/energy-returns.controll.js');
+const energyReturnsController = require('../controllers/energy-returns.controller.js');
 //const { verifyToken } = require('../middleware/auth.middleware.js');
 
 router.get('/', energyReturnsController.getEnergyReturns);
