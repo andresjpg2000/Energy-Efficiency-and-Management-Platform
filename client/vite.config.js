1aeef7245bab5e352e64b019028bc49dcff576df
FabioCorreia99 <Fabio1999C@gmail.com>
2025-06-18 16:24:30 +0100

    feat: add gzip compression support and update dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
